349474616b6fa82898a02f50cb5830791f23ef24
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-02-01 21:12:57 +0530

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,14 @@
 
 const contacts = [
     {
+        id: 1,
+        name: "Sachin Dsilva",
+        imgURL: "https://avatars.githubusercontent.com/u/110468373?v=4",
+        phone: "+1254 577 586",
+        email: "letscode16@gmail.com"
+    },
+    {
+        id: 2,
         name: "Beyonce",
         imgURL: "https://resizing.flixster.com/JaUKifvSvWoMNIpsBLQD3uAOG4M=/218x280/v2/https://flxt.tmsimg.com/assets/221587_v9_bc.jpg",
         phone: "+1230 456 789",
@@ -10,6 +18,7 @@ const contacts = [
     },
 
     {
+        id: 3,
         name: "ColdPlay",
         imgURL: "https://media1.popsugar-assets.com/files/thumbor/rfX4tM_xO_jdVlM1xON3vDMwTMA/32x75:2123x2166/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2020/03/17/853/n/1922398/dd07b6ea5e7125236ac023.26390298_/i/Chris-Martin.jpg",
         phone: "+1264 458 785",
@@ -17,10 +26,12 @@ const contacts = [
 
     },
     {
+        id: 4,
         name: "Jenna Ortega",
         imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH7MnWJZ1maCNtX4kc10DiM8VGA0T8DUblEA&usqp=CAU",
         phone: "+4578 577 475",
         email: "JennaOrtega@FlashMail.com"
     }
+   
 ];
 export default contacts;
