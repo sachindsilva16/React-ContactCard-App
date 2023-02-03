572c431c22ce8a5847756601a7e474448273aefc
Sachin Ronson Dsilva <110468373+sachindsilva16@users.noreply.github.com>
2023-02-04 00:13:36 +0530

    Implementation of js-arrow function

--- src/JSXComponents/App.jsx
+++ src/JSXComponents/App.jsx
@@ -2,17 +2,8 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contact";
 
-function CreateCard(contact) {
-    return (
-        <Card
-            key={contact.id}
-            name={contact.name}
-            imgURL={contact.imgURL}
-            phone={contact.phone}
-            email={contact.email}
-        />
-    );
-}
+//Modified version of below code by using arrow functions
+
 
 
 function App() {
@@ -20,34 +11,72 @@ function App() {
     return (
         <div>
             <h1>My Contacts</h1>
-
-            {contacts.map(CreateCard)}
-
-
-
-
-            {/* <Card name={contacts[3].name}
-                imgURL={contacts[3].imgURL}
-                phone={contacts[3].phone}
-                email={contacts[3].email} />
-
-            <Card name={contacts[0].name}
-                imgURL={contacts[0].imgURL}
-                phone={contacts[0].phone}
-                email={contacts[0].email} />
-
-            <Card name={contacts[1].name}
-                imgURL={contacts[1].imgURL}
-                phone={contacts[1].phone}
-                email={contacts[1].email} />
-
-            <Card name={contacts[2].name}
-                imgURL={contacts[2].imgURL}
-                phone={contacts[2].phone}
-                email={contacts[2].email} /> */}
+            {contacts.map((contact) => {
+                return (
+                    <Card
+                        key={contact.id}
+                        name={contact.name}
+                        imgURL={contact.imgURL}
+                        phone={contact.phone}
+                        email={contact.email}
+                    />
+                );
+            })}
         </div>
     );
 }
+
+
+
+// -----------------------------------******-----------------------------
+//METHOD 2 :(-----------ORIGINAL VERSION--------------)
+
+// function CreateCard(contact) {
+//     return (
+//         <Card
+//             key={contact.id}
+//             name={contact.name}
+//             imgURL={contact.imgURL}
+//             phone={contact.phone}
+//             email={contact.email}
+//         />
+//     );
+// }
+
+
+// function App() {
+
+//     return (
+//         <div>
+//             <h1>My Contacts</h1>
+
+//             {contacts.map(CreateCard)}
+
+
+
+
+//             {/* <Card name={contacts[3].name}
+//                 imgURL={contacts[3].imgURL}
+//                 phone={contacts[3].phone}
+//                 email={contacts[3].email} />
+
+//             <Card name={contacts[0].name}
+//                 imgURL={contacts[0].imgURL}
+//                 phone={contacts[0].phone}
+//                 email={contacts[0].email} />
+
+//             <Card name={contacts[1].name}
+//                 imgURL={contacts[1].imgURL}
+//                 phone={contacts[1].phone}
+//                 email={contacts[1].email} />
+
+//             <Card name={contacts[2].name}
+//                 imgURL={contacts[2].imgURL}
+//                 phone={contacts[2].phone}
+//                 email={contacts[2].email} /> */}
+//         </div>
+//     );
+// }
 
 
 
